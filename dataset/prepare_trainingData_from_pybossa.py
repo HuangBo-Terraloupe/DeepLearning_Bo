65d516a8c6f862e5bd6b855f5e233b55925e6ee9
IsabelHornig <isabel.hornig@terraloupe.com>
2018-08-28 11:25:33 +0200

    medicine classification

--- dataset/prepare_trainingData_from_pybossa.py
+++ dataset/prepare_trainingData_from_pybossa.py
@@ -1,11 +1,13 @@
 import os
+import cv2
 import json
 import pickle
 import geopandas as gpd
 from glob import glob
+import click
 
 
-def generate_gt_from_pybossa(geojson_file, metadata, img_folder, save_folder_annotation, category_type='most_common'):
+def generate_gt_from_pybossa(geojson_file, metadata, img_folder, save_folder_annotation, category_type='most_common', img_save_folder=None):
 
     # load geojson data
     geo_data = gpd.read_file(geojson_file)
@@ -20,36 +22,58 @@ def generate_gt_from_pybossa(geojson_file, metadata, img_folder, save_folder_ann
     img_list = glob(img_folder + '*.jpg')
     img_list.sort()
 
-    for idx, _ in enumerate(img_list):
+    for idx, image_name in enumerate(img_list[0:100]):
         print(idx)
-        annotation_data = {"img_name": geo_data['patch_id'][idx] + '.jpg',
-                           'bboxes': []
-                           }
 
         real_coor = metadata[key_list[idx]]['geometry'].bounds
         fwd = metadata[key_list[idx]]['patch_affine']
         rev = ~fwd
-        tl = rev * (real_coor[0], real_coor[1])
-        br = rev * (real_coor[2], real_coor[3])
+        tl = rev * (real_coor[0], real_coor[3])
+        br = rev * (real_coor[2], real_coor[1])
+
         x1 = int(tl[0])
         y1 = int(tl[1])
         x2 = int(br[0])
         y2 = int(br[1])
 
-        annotation_data['bboxes'].append(
-            {'category': int(geo_data[category_type][idx]),
-             'x1': x1,
-             'x2': x2,
-             'y1': y1,
-             'y2': y2,
-             })
-        with open(os.path.join(save_folder_annotation, geo_data['patch_id'][idx] + '.json'), 'w') as fp:
-            json.dump(annotation_data, fp)
+        # check if the coordinates are correct
+        image = cv2.imread(image_name)
+        img_width = image.shape[1]
+        img_height = image.shape[0]
+
+        if y1 > img_height or y2 > img_height or x1 > img_width or x2 > img_width:
+            print('the bounding box is cross the boundary of image! skip this image.')
+            continue
+        if x1 >= x2 or y1 >= y2:
+            print('the coordinates are fliped! skip this image.')
+            continue
+
+        if save_folder_annotation is not None:
+            annotation_data = {"img_name": geo_data['patch_id'][idx] + '.jpg',
+                               'bboxes': []
+                               }
+
+            annotation_data['bboxes'].append(
+                {'category': int(geo_data[category_type][idx]),
+                 'x1': x1,
+                 'x2': x2,
+                 'y1': y1,
+                 'y2': y2,
+                 })
+            with open(os.path.join(save_folder_annotation, geo_data['patch_id'][idx] + '.json'), 'w') as fp:
+                json.dump(annotation_data, fp)
+
+        if img_save_folder is not None:
+            cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 3)
+            img_save_path = os.path.join(img_save_folder, geo_data['patch_id'][idx] + '.jpg')
+            cv2.imwrite(img_save_path, image)
 
 
 if __name__ == '__main__':
-    geojson_file = '/home/huangbo/Building_damage/port_lavaca/output/port_lavaca.geojson'
-    metadata = '/home/huangbo/Building_damage/port_lavaca/port_lavaca_patches/metadata/patch_geoinfo.pickle'
-    img_folder = '/home/huangbo/Building_damage/port_lavaca/port_lavaca_patches/patches/'
-    save_folder_annotation = '/home/huangbo/Building_damage/port_lavaca/port_lavaca_patches/annotations/'
-    generate_gt_from_pybossa(geojson_file, metadata, img_folder, save_folder_annotation, category_type='most_common')
+    geojson_file = '/home/terraloupe/Building_damage/Keys/output/keys.geojson'
+    metadata = '/home/terraloupe/Building_damage/Keys/metadata/patch_geoinfo.pickle'
+    img_folder = '/home/terraloupe/Building_damage/Keys/patches/'
+    save_folder_annotation = None #'/home/huangbo/Building_damage/port_lavaca/port_lavaca_patches/annotations/'
+    category_type = 'most_common'
+    img_save_folder = '/home/terraloupe/Desktop/Building_train_samples/keys'
+    generate_gt_from_pybossa(geojson_file, metadata, img_folder, save_folder_annotation, category_type, img_save_folder)
---
+++ medicine_classification.py
@@ -0,0 +1,140 @@
+import os
+import cv2
+import numpy as np
+
+from time import time
+from glob import glob
+from PIL import Image
+
+from keras.models import Model
+from keras.utils import np_utils
+from keras.applications import VGG16
+from keras.layers import Input, Flatten, Dense
+from keras.metrics import categorical_crossentropy
+from keras.callbacks import ModelCheckpoint, TensorBoard
+
+from keras.layers import Dense, Activation, Convolution2D, MaxPooling2D, Flatten
+from keras.optimizers import Adam
+
+
+def read_image_bgr(path):
+    """ Read an image in BGR format.
+    Args
+        path: Path to the image.
+    """
+    image = np.asarray(Image.open(path).convert('RGB'))
+    return image[:, :, ::-1].copy()
+
+def normalization(x):
+    x[..., 0] -= 103.939
+    x[..., 1] -= 116.779
+    x[..., 2] -= 123.68
+
+    return x/127.5
+
+
+def medicine_classifier_training_run(train_folder, test_folder, image_width, image_hight, model_save_folder):
+
+    # class_mapping
+    class_mapping = {}
+    for i, category in enumerate(os.listdir(train_folder)):
+        class_mapping[category] = i
+    print('The class mapping is', class_mapping)
+    print('The number of total classes are:', len(class_mapping))
+
+    # training and testing data statistics
+    num_train_samples = 0
+    for img_folder in os.listdir(train_folder):
+        num_train_samples = num_train_samples + len(glob(os.path.join(train_folder, img_folder) + "/*.jpg"))
+        num_train_samples = num_train_samples + len(glob(os.path.join(train_folder, img_folder) + "/*.JPG"))
+    print('total number of training samples are:', num_train_samples)
+
+    num_test_samples = 0
+    for img_folder in os.listdir(test_folder):
+        num_test_samples = num_test_samples + len(glob(os.path.join(test_folder, img_folder) + "/*.jpg"))
+        num_test_samples = num_test_samples + len(glob(os.path.join(test_folder, img_folder) + "/*.JPG"))
+    print('total number of testing samples are:', num_test_samples)
+
+    # load training data
+    train_images = np.zeros([num_train_samples, image_width, image_hight, 3])
+    train_labels = np.zeros(num_train_samples, dtype=int)
+    train_data_index = 0
+
+    for category in os.listdir(train_folder):
+        img_folder_path = os.path.join(train_folder, category)
+        image_paths = os.listdir(img_folder_path)
+        for image_path in image_paths:
+            image = read_image_bgr(os.path.join(img_folder_path, image_path))
+            image = cv2.resize(image, (image_hight, image_width))
+            train_images[train_data_index] = image
+            train_labels[train_data_index] = int(class_mapping[category])
+            train_data_index = train_data_index + 1
+
+    # load testing data
+    test_images = np.zeros([num_test_samples, image_width, image_hight, 3])
+    test_labels = np.zeros(num_test_samples, dtype=int)
+    test_data_index = 0
+
+    for category in os.listdir(test_folder):
+        img_folder_path = os.path.join(test_folder, category)
+        image_paths = os.listdir(img_folder_path)
+        for image_path in image_paths:
+            image = read_image_bgr(os.path.join(img_folder_path, image_path))
+            image = cv2.resize(image, (image_hight, image_width))
+            test_images[test_data_index] = image
+            test_labels[test_data_index] = int(class_mapping[category])
+            test_data_index = test_data_index + 1
+
+    # training and test data pre-processing
+    train_labels = np_utils.to_categorical(train_labels, num_classes=len(class_mapping))
+    test_labels = np_utils.to_categorical(test_labels, num_classes=len(class_mapping))
+
+    train_images = normalization(train_images)
+    test_images = normalization(test_images)
+
+    # training model
+    base_model = VGG16(input_shape=(image_width, image_hight, 3), weights='imagenet', include_top=False)
+    x = base_model.layers[-1].output
+    x = Flatten(name='flatten')(x)
+    x = Dense(4096, activation='relu', name='fc1')(x)
+    x = Dense(4096, activation='relu', name='fc2')(x)
+    prediction = Dense(len(class_mapping), activation='softmax', name='predictions')(x)
+
+    model = Model(inputs=base_model.input, outputs=prediction)
+    print(model.summary())
+
+    # optimizer
+    adam = Adam(lr=1e-4)
+
+    # compile the model
+    model.compile(optimizer=adam,
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    # callbacks
+    filepath = os.path.join(model_save_folder, 'model.h5py')
+    weights = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=True,
+                              mode='auto', period=1)
+    tensorboard = TensorBoard(log_dir=os.path.join(model_save_folder, "logs").format(time()))
+
+    print('start training ...')
+    # Another way to train the model
+    model.fit(train_images, train_labels, epochs=5, batch_size=1, callbacks=[weights, tensorboard])
+
+    print('\nTesting ------------')
+
+    # Evaluate the model with the metrics
+    loss, accuracy = model.evaluate(test_images, test_labels)
+
+    print('\ntest loss: ', loss)
+    print('\ntest accuracy: ', accuracy)
+
+
+if __name__ == '__main__':
+    train_folder = '/home/terraloupe/DataSet/Train'
+    test_folder = '/home/terraloupe/DataSet/Test'
+    image_width = 449
+    image_hight = 449
+    model_save_folder = '/home/terraloupe/DataSet/output'
+    medicine_classifier_training_run(train_folder, test_folder, image_width, image_hight, model_save_folder)
+
