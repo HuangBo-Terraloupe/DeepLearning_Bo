22fd7c313f7c0be22cead68645975bcdf0f308e7
IsabelHornig <isabel.hornig@terraloupe.com>
2018-10-18 18:17:55 +0200

    'here training'

--- dataset/create_yml_file.py
+++ dataset/create_yml_file.py
@@ -22,11 +22,11 @@ GT_TYPE = "semseg" # semseg, detection, bbox
 
 PREFIX = '/data/here/'
 
-IMAGE_DIR_NAME = 'texas_thin/images'
-MASKS_DIR_NAME = 'texas_thin/masks'  #Only name, not full path
+IMAGE_DIR_NAME = 'texas2_thin/images'
+MASKS_DIR_NAME = 'texas2_thin/masks'  #Only name, not full path
 IMG_EXT = '.png'  #Images extensions
 MASKS_EXT = '.png'  #Masks extensions
-OUTFILE_NAME = '/data/here/texas1.yml'
+OUTFILE_NAME = '/data/here/texas2.yml'
 TRAIN_RATIO = [1, 0.0, 0.0]  # Training set ratio  Between 0-1
 
 N_categories = 3
